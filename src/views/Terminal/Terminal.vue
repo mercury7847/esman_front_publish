b6dccfae0ad7818d9adb0f267e31a6c581681ebf
Jeongok <evagreen12011@gmail.com>
2021-10-19 10:19:07 +0900

    소스 이전 누락 파일 추가 - 터미널, 영업점 조회

--- src/views/Terminal/Terminal.vue
+++ src/views/Terminal/Terminal.vue
@@ -3,32 +3,46 @@
   <Gnb />
   <div class="terminal">
     <div class="l-inner">
-      <div class="l-inp">
+      <div class="l-inp l-search">
         <div class="inp">
           <div class="inp-inner">
             <input type="text" class="input-text-line" placeholder="주소를 입력하세요." id="inp" autofocus>
           </div>
-          <button class="btn search-btn"><i class="icon-search"></i></button>
+          <button class="btn btn-search"><i class="icon-search small"></i></button>
         </div>
       </div>
 
-      <div class="d-flex-be-center">
+      <div class="d-flex-be-center" v-show="!searchResult">
         <p class="txt-sub">최근 검색어</p>
         <button class="btn small-size white">전체 삭제</button>
       </div>
-    </div>
-      <div class="address-list">
-        <ul>
-          <li>
-            <div class="address-txt">
-              <p class="address-title">구로</p>
-            </div>
-            <div class="address-btn" v-show="!isSelect">
-              <button class="btn-delete"></button>
-            </div>
-          </li>
-        </ul>
+      <div class="d-flex-be-center" v-show="searchResult">
+        <p class="txt-sub">검색결과 <span class="txt-primary">4</span></p>
+        <button class="btn small-size white">전체 삭제</button>
       </div>
+    </div>
+    <div class="address-list" v-show="!searchResult">
+      <ul>
+        <li v-for="item in recentSearchList" :key="item">
+          <div class="address-txt">
+            <p class="address-title txt-weight-normal">{{ item.title }}</p>
+          </div>
+          <div class="address-btn">
+            <button class="btn-delete"></button>
+          </div>
+        </li>
+      </ul>
+    </div>
+    <div class="address-list" v-show="searchResult">
+      <ul>
+        <li v-for="item in searchList" :key="item">
+          <div class="address-txt">
+            <p class="address-title" v-html="item.title"></p>
+            <p class="address-desc" v-html="item.address"></p>
+          </div>
+        </li>
+      </ul>
+    </div>
   </div>
 </div>
 </template>
@@ -37,15 +51,47 @@
 import Gnb from '../../components/layout/Gnb'
 export default {
   components: { Gnb },
-  name:'Address',
+  name:'Terminal',
   data() {
    return {
-    isSelect: false,
+    searchResult: true,
+    recentSearchList: [
+      {
+        title: '구로',
+      },
+      {
+        title: '고양시 덕양구',
+      },
+      {
+        title: '용산구 청파동',
+      },
+      {
+        title: '청파동',
+      },
+    ],
+    searchList: [
+      {
+        title: '한진택배 <span class="txt-primary">구로</span> 터미널',
+        address: '서울특별시 <span class="txt-primary">구로</span>구 가산디지털 2로 83<br>터미널 코드 110',
+      },
+      {
+        title: '한진택배 <span class="txt-primary">구로</span> 1 영업점',
+        address: '서울특별시 <span class="txt-primary">구로</span>구 가산디지털 2로 83',
+      },
+      {
+        title: '한진택배 <span class="txt-primary">구로</span> 2 영업점',
+        address: '서울특별시 <span class="txt-primary">구로</span>구 가산디지털 2로 83',
+      },
+      {
+        title: '한진택배 <span class="txt-primary">구로</span> 3 영업점',
+        address: '서울특별시 <span class="txt-primary">구로</span>구 가산디지털 2로 83',
+      },
+    ],
    };
   },
   methods: {
-    switchSelect: function () {
-      this.isSelect = !this.isSelect;
+    switchSearch: function () {
+      this.searchResult = !this.searchResult;
     },
   },
 }
