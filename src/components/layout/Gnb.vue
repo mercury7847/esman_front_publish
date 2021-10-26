a3172a303b9aa1996b7c13602358d9e97d2aa292
Jeongok <evagreen12011@gmail.com>
2021-10-26 16:24:27 +0900

    1026 GNB 수정, 폰트사이즈 rem으로 변경 완료, 햄버거 메뉴 전체완료

--- src/components/layout/Gnb.vue
+++ src/components/layout/Gnb.vue
@@ -4,44 +4,36 @@
       <div class="user-thumb">
         <img src="../../assets/images/user.png" alt="">
       </div>
-
       <div class="user-info">
         <span class="user-name">김한진</span>
         <span class="user-addr">용산2</span>
       </div>
     </div>
-
     <div class="nivigation" v-else-if="headerStyle == 'navi'">
         <div class="navi-action">
-          <button class="prev-btn" @click="back">뒤로</button>
+          <button class="btn btn-prev" @click="back">뒤로</button>
         </div>
         <div class="subject">
           <h2><span>{{$route.name}}</span></h2>
         </div>
     </div>
-
-
     <div class="global-state">
-      <div class="icon-bluetooth">
-        <button></button>
-      </div>
-      <div class="icon-alram">
-        <button></button>
-        <span class="alram-count"><span class="alram-count-num">2</span></span>
-      </div>
-      <div class="icon-menu">
-        <button></button>
-        <!-- <span class="global-state-count"></span> -->
-      </div>
+      <button class="btn btn-bluetooth"></button>
+      <button class="btn btn-alarm"><span class="alram-count-num badge-circle-blue">2</span></button>
+      <button class="btn btn-menu" @click="sideMenuOn = true"></button>
     </div>
+    <side-menu v-if="sideMenuOn" @click="toggleSideMenu"/>
   </div>
 </template>
 <script>
+import SideMenu from '../../components/layout/SideMenu'
 export default {
+  components: { SideMenu },
   name:'Gnb',
   data() {
     return {
-      headerStyle:''
+      headerStyle:'',
+      sideMenuOn: false,
     }
   },
   mounted() {
@@ -56,13 +48,12 @@ export default {
   methods: {
     back(){
       this.$router.back();
-    }
+    },
+    toggleSideMenu: function () {
+      this.sideMenuOn = !this.sideMenuOn;
+    },
   }
 }
 
 </script>
-<style   scoped>
-/*@import "../../assets/scss/var.scss";
-@import "../../assets/scss/layout/layout.scss";*/
-</style>
 
--- src/views/Login/Join.vue
+++ src/views/Login/Join.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="asdfsdf">
+  <div class="l-service">
     <Gnb />
     <div class="join">
       <el-tabs v-model="activeName" @tab-click="handleClick">
