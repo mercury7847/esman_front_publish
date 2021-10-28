150d3a4defe4ba444bc009d8ebbdb8559a839091
Jeongok <evagreen12011@gmail.com>
2021-10-28 17:17:00 +0900

    gnb 공통 알림 , 실시간알림(일일집배송정보) 팝업

--- src/components/layout/Gnb.vue
+++ src/components/layout/Gnb.vue
@@ -19,21 +19,76 @@
     </div>
     <div class="global-state">
       <button class="btn btn-bluetooth"></button>
-      <button class="btn btn-alarm"><span class="alram-count-num badge-circle-blue">2</span></button>
-      <button class="btn btn-menu" @click="sideMenuOn = true"></button>
+      <button class="btn btn-alarm" @click="toggleDrawer"><span class="alram-count-num badge-circle-blue">2</span></button>
+      <button class="btn btn-menu" @click="toggleSideMenu"></button>
     </div>
     <side-menu v-if="sideMenuOn" @click="toggleSideMenu"/>
+
+    <!-- 알림 -->
+    <div class="l-notification l-drawer l-scroll">
+      <el-drawer
+        :visible.sync="drawerNoti"
+        direction="btt"
+        size="92%"
+        :with-header="false"
+        :modal="false">
+        <div class="drawer-title d-flex-be-center">
+          <span>알림 <span class="txt-primary">4</span></span>
+          <button class="btn small-size white">알림 모두 지우기</button>
+        </div>
+        <ul class="l-list scroll-y">
+          <li v-for="noti in notiList" :key="noti.title">
+            <div class="noti-list" @click="popVisible">
+              <p class="ft-color-black">
+                <span class="ft-weight-bold mr-10">{{ noti.title }}</span>
+                <span class="txt-sub ft-size-xsmall">{{ noti.time }}</span>
+              </p>
+              <p class="mt-5 txt-sub ft-size-xsmall">{{ noti.desc }}</p>
+            </div>
+            <button class="btn-delete"></button>
+          </li>
+        </ul>
+      </el-drawer>
+    </div>
+
+    <pop-notification v-if="popOn" @click="popVisible" />
+
   </div>
 </template>
 <script>
 import SideMenu from '../../components/layout/SideMenu'
+import PopNotification from '../../views/Modal/PopNotification'
 export default {
-  components: { SideMenu },
+  components: { SideMenu, PopNotification },
   name:'Gnb',
   data() {
     return {
       headerStyle:'',
       sideMenuOn: false,
+      drawerNoti: false,
+      popOn: false,
+      notiList: [
+        {
+          title: '오늘의 배송물량',
+          time: '1시간 전',
+          desc: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+        },
+        {
+          title: '오늘의 집하 물량',
+          time: '1시간 전',
+          desc: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+        },
+        {
+          title: '오늘의 기상 현황',
+          time: '2시간 전',
+          desc: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+        },
+        {
+          title: '인근 반품 건',
+          time: '(화) 오전 7:45분',
+          desc: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+        },
+      ]
     }
   },
   mounted() {
@@ -51,6 +106,12 @@ export default {
     },
     toggleSideMenu: function () {
       this.sideMenuOn = !this.sideMenuOn;
+    },
+    toggleDrawer: function () {
+      this.drawerNoti = !this.drawerNoti;
+    },
+    popVisible: function () {
+      this.popOn = !this.popOn;
     },
   }
 }
---
+++ src/views/Modal/PopNotification.vue
@@ -0,0 +1,60 @@
+<template>
+  <!-- 실시간 알림(일일 집배송 정보) -->
+  <div class="">
+    <el-dialog
+      title="Today’s Report"
+      :visible.sync="popNotification"
+      :append-to-body="true"
+      class="l-dialog small l-notification-dialog"
+      width="300px"
+      center>
+      <div class="card pale-gray">
+        <div class="card-inner">
+          <div class="d-flex-be-center">
+            <div>
+              <p><span class="txt-primary">김한진</span> 기사님! 8월 23일 (월)의<br>집배송 물량은 다음과 같습니다.</p>
+              <div class="mt-10">
+                <span class="badge green">배송</span><span class="ft-weight-bold">5</span>
+                <span class="badge yellow ml-20">집하</span><span class="ft-weight-bold">12</span>
+              </div>
+            </div>
+            <button class="btn"><i class="icon-sm icon-arrow-r"></i></button>
+          </div>
+        </div>
+        <div>
+        </div>
+      </div>
+      <div class="card pale-gray mt-10">
+        <div class="card-inner">
+          <div class="d-flex-be-center">
+            <div>
+              <span>오늘의 새 소식 </span><span class="txt-primary">1</span><span>건이 있습니다.</span>
+            </div>
+            <button class="btn"><i class="icon-sm icon-arrow-r"></i></button>
+          </div>
+        </div>
+        <div>
+        </div>
+      </div>
+      <div slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="popClick">닫기</el-button>
+      </div>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'popNotification',
+  data() {
+    return {
+      popNotification: true,
+    }
+  },
+  methods: {
+    popClick: function () {
+      this.$emit('click')
+    },
+  }
+}
+</script>
