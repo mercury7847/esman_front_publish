2d340cf22756bcc00134b8915bfcfbf2c11fddb9
jeongok <evagreen12011@gmail.com>
2021-11-30 10:45:29 +0900

    landing page 로딩 추가

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,26 +1,28 @@
 <template>
-  <div class="landing">
+  <div class="landing" v-loading="loading" element-loading-spinner="el-icon-loading" element-loading-background="rgba(0, 0, 0, 0.3)">
     <div class="logo">
-      <img src="@/assets/images/lending_logo.png" alt="">
+      <img src="@/assets/images/logo-white.svg" alt="" />
       <p class="slogun"><span>소비자 중심경영 인증기업</span></p>
     </div>
     <div class="welcom-message">
-      <p>WELCOME  — 기쁨 전하는 행복상자 한진택배</p>
+      <p>기쁨 전하는 행복상자 한진택배</p>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name:'Landing',
+  name: "Landing",
+  data() {
+    return {
+      loading: true,
+    };
+  },
 
-  mounted(){
-    setTimeout(()=>{
-      this.$router.push("/Login")
-    }, 2 * 1000)
-
-  }
-}
+  mounted() {
+    setTimeout(() => {
+      this.$router.push("/Login");
+    }, 2 * 1000);
+  },
+};
 </script>
-
-
