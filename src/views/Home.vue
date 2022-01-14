10cc29088f97de240c77c9d2190e1a81ecdf2d6e
jeongok <evagreen12011@gmail.com>
2022-01-14 13:32:37 +0900

    공통 컴포넌트 수정중

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="l-inner mt-30">
     <p class="title-page mb-30">퍼블 완료 페이지 목록</p>
-    <router-link to="/guide" class="btn btn-size-large primary mb-30">공통 컴포넌트</router-link>
+    <div class="mb-30"><router-link to="/guide" class="btn btn-size-large primary">공통 컴포넌트</router-link></div>
+    <div class="mb-30"><router-link to="/" class="btn btn-size-large primary">스플래시</router-link></div>
     <div class="mb-30">
       <router-link to="/login" class="btn btn-size-large primary">로그인</router-link>
       <ul class="title mt-20">
@@ -12,14 +13,17 @@
         <li class="mb-10">사번찾기팝업,</li>
         <li class="mb-10">비밀번호재발급 팝업,</li>
         <li class="mb-10">최초 로그인시 비밀번호 설정팝업</li>
-        <li class="mb-10">미완료: 위치정보 제공동의 상세보기 팝업 (기획없음)</li>
+        <li class="mb-10 ft-color-grey">미완료: 위치정보 제공동의 상세보기 팝업 (기획없음)</li>
       </ul>
     </div>
-    <div>
-      <router-link to="/delivery-start" class="btn btn-size-large primary">집배송 출발</router-link>
+    <div class="mb-30">
+      <router-link to="/dashboard" class="btn btn-size-large white">홈</router-link>
     </div>
-    <div>
-      <router-link to="/delivery-processing"><el-button>집배송 처리</el-button></router-link>
+    <div class="mb-30">
+      <router-link to="/delivery-start" class="btn btn-size-large white">집배송 출발</router-link>
+    </div>
+    <div class="mb-30">
+      <router-link to="/delivery-processing" class="btn btn-size-large white">집배송 처리</router-link>
     </div>
   </div>
 </template>
